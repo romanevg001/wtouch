fcda0446f6eff96117bb23bccce7a1783bb7d4c1
Natali Donova <romanevg001@gmail.com>
2018-07-02 13:38:42 +0300

    +

--- src/app/components/quiz/cquiz/wayToEstimate.ts
+++ src/app/components/quiz/cquiz/wayToEstimate.ts
@@ -20,7 +20,7 @@ export class WayToEstimate {
     }
 }
 
-class OneTry implements IWayToEstimate {
+let OneTry = class OneTry implements IWayToEstimate {
 
     constructor(private historyAnswers: Answers[]){    }
     
@@ -28,6 +28,7 @@ class OneTry implements IWayToEstimate {
         return (this.historyAnswers[this.historyAnswers.length-1].valid) ? 10 : 0 ;
     }
 }
+
 
 class SeveralTries implements IWayToEstimate {
 
@@ -39,4 +40,38 @@ class SeveralTries implements IWayToEstimate {
     }
 }
 
+
+class MyClass {
+    value;
+    static get [Symbol.species]() {
+        return this;
+    }
+    constructor(value) {
+        this.value = value;
+    }
+    clone() {
+        console.log('this.constructor',this.constructor)
+        console.log('Symbol.species', Symbol.species)
+        console.log(new this.constructor[Symbol.species](this.value))
+        return new this.constructor[Symbol.species](this.value);
+    } 
+}
+
+class MyDerivedClass1 extends MyClass {
+    // пустой
+}
+class MyDerivedClass2 extends MyClass {
+    static get [Symbol.species]() {
+      //  console.log(MyClass)
+        return MyClass;
+    }
+}
+let instance1 = new MyDerivedClass1("foo"),
+    clone1 = instance1.clone(),
+    instance2 = new MyDerivedClass2("bar"),
+    clone2 = instance2.clone();
+console.log(clone1 instanceof MyClass);
+console.log(clone1 instanceof MyDerivedClass1);
+console.log(clone2 instanceof MyClass);
+console.log(clone2 instanceof MyDerivedClass2);
 
